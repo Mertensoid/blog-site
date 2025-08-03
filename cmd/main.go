e319dcccfb0e1d1b04896a484d9c98ae80016f04
Mertensoid <matsoukm@yandex.ru>
2025-08-03 08:00:02 +0300

    logger conf

--- cmd/main.go
+++ cmd/main.go
@@ -2,10 +2,13 @@ package main
 
 import (
 	"blog-site/config"
+	"blog-site/package/logger"
 )
 
 func main() {
 	config.Init()
 	dbConf := config.NewDatabaseConfig()
 	loggerConf := config.NewLogConfig()
+
+	logger := logger.NewLogger(loggerConf)
 }
