d57755100289b7491a10c644933ea609993abd2c
Mertensoid <matsoukm@yandex.ru>
2025-08-03 08:15:06 +0300

    version conflict fix

--- cmd/main.go
+++ cmd/main.go
@@ -3,6 +3,9 @@ package main
 import (
 	"blog-site/config"
 	"blog-site/package/logger"
+
+	"github.com/gofiber/contrib/fiberzerolog"
+	"github.com/gofiber/fiber/v2"
 )
 
 func main() {
@@ -11,4 +14,12 @@ func main() {
 	loggerConf := config.NewLogConfig()
 
 	logger := logger.NewLogger(loggerConf)
+
+	app := fiber.New()
+	app.Use(fiberzerolog.New(fiberzerolog.Config{
+		Logger: logger,
+	}))
+
+	logger.Info().Msg(dbConf.Url)
+
 }
--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.24.1
 
 require (
 	github.com/andybalholm/brotli v1.2.0 // indirect
+	github.com/gofiber/contrib/fiberzerolog v1.0.3 // indirect
 	github.com/gofiber/fiber/v2 v2.52.9 // indirect
 	github.com/gofiber/fiber/v3 v3.0.0-beta.5 // indirect
 	github.com/gofiber/schema v1.6.0 // indirect
